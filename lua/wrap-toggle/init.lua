fb58f9686892d88239fd92966296787cdb80895a
Derek Tata <derekjtata@gmail.com>
2023-10-25 16:26:53 -0400

    Updated wrap-toggle.nvim
    • Simplified codebase
    • Made keybind customizable

--- lua/wrap-toggle/init.lua
+++ lua/wrap-toggle/init.lua
@@ -1,46 +1,32 @@
--- TODO: Rewrite the function to check current wrap status using ':set wrap ?',
--- and implement logic based on the current setting.
--- NOTES: Will do this at a later time. This hacky solution seems to work for the moment.
+local function setup(config)
+  -- Set default values if not provided in the config
+  config = config or {}
+  local key_bind = config.key_bind or "<A-z>"
 
-local o = vim.opt
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+  local map = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
 
-local function setup()
-  -- Keybind
-  map("n", "<A-z>", ":lua Toggle_wrap()<CR>", opts)
+  -- Use the key binding from the config or the default
+  map("n", key_bind, ":lua Toggle_wrap()<CR>", opts)
 end
 
--- Keep track of how many times we've pressed the key
-local count = 0
-local max_count = 1
-
 function Enable_wrap()
-  count = count + 1
-  if count > max_count then
-    count = max_count
-  end
-  o.wrap = true
-  o.linebreak = true
+  vim.opt.wrap = true
+  vim.opt.linebreak = true
   print "Wrap enabled"
 end
 
 function Disable_wrap()
-  count = count - 1
-  if count < 0 then
-    count = 0
-  end
-  o.wrap = false
-  o.linebreak = false
+  vim.opt.wrap = false
+  vim.opt.linebreak = false
   print "Wrap disabled"
 end
 
 function Toggle_wrap()
-  -- When the count is 0, we're in the default mode
-  if count == 0 then
-    Enable_wrap()
-  else
+  if vim.opt.wrap:get() then
     Disable_wrap()
+  else
+    Enable_wrap()
   end
 end
 
